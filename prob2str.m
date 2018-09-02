137d82bd0c09af2110c5907fe96e86d660d88e74
index-coding <42898026+index-coding@users.noreply.github.com>
2018-09-01 18:43:00 -0700

    Update prob2str.m

--- prob2str.m
+++ prob2str.m
@@ -3,7 +3,6 @@ function probstr = prob2str(prob)
 % Convert a problem in the form {A1, A2, ... ,An} to a human-readable string
 % For example, {[2,3],[3],[1]} generates (1|2,3),(2|3),(3|1)
 
-% Problem to side information graph Gd
   n = length(prob);
   probstr = "(";
   for i = 1:length(prob)
